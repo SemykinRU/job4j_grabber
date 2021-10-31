411e8298316c085fb8c6fdf4ac4781ddb2479146
Roman Semykin <KindPanic@mail.ru>
2021-11-01 00:13:12 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -5,8 +5,23 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
+
 public class SqlRuParse {
+
+    public String description(String url) throws IOException {
+        Document doc = Jsoup.connect(url).get();
+        Elements row = doc.select(".msgBody");
+        String str = "";
+        for (var s : row) {
+            str = s.parent().child(1).text();
+            break;
+        }
+        return str;
+    }
+
     public static void main(String[] args) throws Exception {
+        SqlRuParse sqlRuParse = new SqlRuParse();
         String url = "https://www.sql.ru/forum/job-offers";
         for (int i = 1; i < 6; i++) {
             Document doc = Jsoup.connect(String.format("%s/%d", url, i)).get();
@@ -16,6 +31,7 @@ public class SqlRuParse {
                 System.out.println(href.attr("href"));
                 System.out.println(href.text());
                 System.out.println(td.parent().child(5).text());
+                System.out.println(sqlRuParse.description(href.attr("href")));
             }
         }
     }
