e0ffd5f297ef536d27f704e805d78e49b7469a9b
Roman Semykin <KindPanic@mail.ru>
2021-11-01 14:10:05 +0300

    2.2. Модель данных - Post. [#285211 #223041]

--- src/main/java/ru/job4j/html/PostModel.java
+++ src/main/java/ru/job4j/html/PostModel.java
@@ -9,6 +9,67 @@ public class PostModel {
     private String link;
     private String description;
     private LocalDateTime created;
+    private LocalDateTime update;
+
+    public PostModel() {
+    }
+
+    public PostModel(int id, String title, String link, String description, LocalDateTime created, LocalDateTime update) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+        this.update = update;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public LocalDateTime getUpdate() {
+        return update;
+    }
+
+    public void setUpdate(LocalDateTime update) {
+        this.update = update;
+    }
 
     @Override
     public boolean equals(Object o) {
